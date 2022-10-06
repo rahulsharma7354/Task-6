bf0b01748ee8d8d2001e43fd2aefe5c6f2e2f7fd
Rahul Sharma <rahul.sharma@Technovert.com>
2022-10-06 19:57:45 +0530

    website

--- javaScript/form.js
+++ javaScript/form.js
@@ -1,67 +1,82 @@
 $(function () {
-    $(".add-item").click(function(){
+    $(".add-item").click(function () {
         $(".person-info").addClass("hide");
         $(".form").addClass("show");
     })
     var personname = [];
-    var personemail=[];
-    var personlandline=[];
-    var personwebsite=[];
-    var personaddress=[];
-    
-    $(".form-submit-button").click(function () {
-        var name = $(".input-name").val();
-        var email = $(".input-email").val();
-        var mobile = $(".input-mobile").val();
-        var landline = $(".input-landline").val();
-        var website = $(".input-website").val();
-        var address = $(".input-address").val();
-        var firstName = $(".first-person-name").val();
-        var secondName = $(".second-person-name").val();
-        var thirdName=$(".third-person-name").val(); 
-        var forthName=$(".forth-person-name").val();
-        if (firstName.length == 0) {
-            personname[0]=name;
-            personemail[0]=email;
-            personlandline[0]=landline;
-            personwebsite[0]=website;
-            personaddress[0]=address;
-            alert[personname[0]];
-            $(".first-person-name").html(name);
-            $(".first-person-email").html(email);
-            $(".first-person-number").html(mobile);
-        }
-        else if (secondName.length == 0) {
-            personname[1]=name;
-            personemail[1]=email;
-            personlandline[1]=landline;
-            personwebsite[1]=website;
-            personaddress[1]=address;
-            $(".second-person-name").html(name)
-            $(".second-person-email").html(email);
-            $(".second-person-number").html(mobile);
-        }
-        else if (thirdName.length == 0) {
-            personname[2]=name;
-            personemail[2]=email;
-            personlandline[2]=landline;
-            personwebsite[2]=website;
-            personaddress[2]=address;
-            $(".third-person-name").html(name);
-            $(".third-person-email").html(email);
-            $(".third-person-number").html(mobile);
-        }
-        else {
-            personname[3]=name;
-            personemail[3]=email;
-            personlandline[3]=landline;
-            personwebsite[3]=website;
-            personaddress[3]=address;
-            $(".fourth-person-name").html(name);
-            $(".fourth-person-email").html(email);
-            $(".fourth-person-number").html(mobile);
-        }
-    })
+    var personemail = [];
+    var personlandline = [];
+    var personwebsite = [];
+    var personaddress = [];
 
-});
+    // $(".form-submit-button").click(function () {
+    // var button =  document.getElementsByClassName('second-button');
+    // for( var  i = 0 ; i< button.length ; i++){
+    //   button[i].addEventListener('click' , function() {
+    //           document.getElementById("myform").reset();
+    //           location.reload();
+    //   })
+    // }
+    var k = $(".input");
+    var btn = $(".form-submit-button");
+    for (var i = 0; i < k.length; i++) {
+        btn.click( function () {
+            var name = $(".input-name").val();
+            var email = $(".input-email").val();
+            var mobile = $(".input-mobile").val();
+            var landline = $(".input-landline").val();
+            var website = $(".input-website").val();
+            var address = $(".input-address").val();
+            var firstName = $(".first-person-name").val();
+            var secondName = $(".second-person-name").val();
+            var thirdName = $(".third-person-name").val();
+            var forthName = $(".forth-person-name").val();
+            if(i==0){
+                $(".first-person-name").html(name);
+            }
 
+        })
+    }
+
+})
+// if (firstName.length == 0) {
+//     personname[0] = name;
+//     personemail[0] = email;
+//     personlandline[0] = landline;
+//     personwebsite[0] = website;
+//     personaddress[0] = address;
+//     alert[personname[0]];
+//     $(".first-person-name").html(name);
+//     $(".first-person-email").html(email);
+//     $(".first-person-number").html(mobile);
+// }
+// else if (secondName.length == 0) {
+//     personname[1] = name;
+//     personemail[1] = email;
+//     personlandline[1] = landline;
+//     personwebsite[1] = website;
+//     personaddress[1] = address;
+//     $(".second-person-name").html(name)
+//     $(".second-person-email").html(email);
+//     $(".second-person-number").html(mobile);
+// }
+// else if (thirdName.length == 0) {
+//     personname[2] = name;
+//     personemail[2] = email;
+//     personlandline[2] = landline;
+//     personwebsite[2] = website;
+//     personaddress[2] = address;
+//     $(".third-person-name").html(name);
+//     $(".third-person-email").html(email);
+//     $(".third-person-number").html(mobile);
+// }
+// else {
+//     personname[3] = name;
+//     personemail[3] = email;
+//     personlandline[3] = landline;
+//     personwebsite[3] = website;
+//     personaddress[3] = address;
+//     $(".fourth-person-name").html(name);
+//     $(".fourth-person-email").html(email);
+//     $(".fourth-person-number").html(mobile);
+// }
